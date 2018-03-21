8352474a44005bc399b305ae4405d0051963b55d
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-03-20 18:05:41 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+'''
+https://stackoverflow.com/a/21530768/3166424
+Need to install python dev
+python setup.py build
+'''
+from distutils.core import setup, Extension, DEBUG
+
+sfc_module = Extension('superfastcode', sources = ['main.cpp'])
+
+setup(name = 'superfastcode',
+    version = '1.0',
+    description = 'Python Package with superfastcode C++ extension',
+    ext_modules = [sfc_module]
+    )
